d5d1c9e9affbfb7df6f71924b2bd15cabe7db1be
masterpg <masterpg@livedeveloper.net>
2020-02-03 14:27:52 +0900

    CORSの除外パターンを変更

--- functions/functions.env.dev.sh
+++ functions/functions.env.dev.sh
@@ -4,6 +4,6 @@ firebase functions:config:set \
   functions.region="asia-northeast1" \
   storage.bucket="gs://vue-base-project-7295.appspot.com/" \
   cors.whitelist="http://localhost, http://localhost:5000, http://localhost:5010, chrome-extension://aejoelaoggembcahagimdiliamlcdmfm" \
-  cors.excludes="GET:^storage/users/"
+  cors.excludes="GET:^storage/"
 
 firebase functions:config:get > ./.runtimeconfig.json
--- functions/functions.env.prod.sh
+++ functions/functions.env.prod.sh
@@ -4,4 +4,4 @@ firebase functions:config:set \
   functions.region="asia-northeast1" \
   storage.bucket="gs://vue-base-project-7295.appspot.com/" \
   cors.whitelist="https://vue-base-project-7295.web.app, https://vue-base-project-7295.firebaseapp.com" \
-  cors.excludes="GET:^storage/users/"
+  cors.excludes="GET:^storage/"
