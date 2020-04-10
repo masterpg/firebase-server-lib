48278638ff7da80f26fa1c97a0a7fd383df1cab0
masterpg <masterpg@livedeveloper.net>
2020-04-11 01:16:47 +0900

    依存パッケージのアップグレード（rev: b84bcee5）の対応漏れがあったので修正

--- functions/src/lib/gql/decorators/context.ts
+++ functions/src/lib/gql/decorators/context.ts
@@ -1,4 +1,12 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common'
 import { GQLContext } from '../../nest'
-import { createParamDecorator } from '@nestjs/common'
 
-export const GQLCtx = createParamDecorator((data, [root, args, ctx, info]) => ctx as GQLContext)
+export const GQLCtx = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const type = ctx.getType() as 'http' | 'graphql'
+
+  if (type === 'graphql') {
+    return ctx.getArgByIndex(2) as GQLContext
+  } else {
+    throw new Error('GQLCtx not be used outside of GraphQL Resolver.')
+  }
+})
